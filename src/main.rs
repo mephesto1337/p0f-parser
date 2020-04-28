96e6e44e3ef7c79ba44c80ae19b4fe9958e082e1
Thomas WACHE <thomas@wache.fr>
2020-04-28 20:12:01 +0200

    Make main to stop at 1st error

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io::{self, BufRead, BufReader};
 
 use p0f_parser::parse_line;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), io::Error> {
     let path = env::args().skip(1).next().unwrap_or("./p0f.log".to_owned());
     let logfile = File::open(path)?;
     let reader = BufReader::new(logfile);
 
     for line in reader.lines() {
-        match parse_line(&line?) {
+        let line = line?;
+        match parse_line(&line) {
             Ok((_rest, p0f)) => println!("{:?}", p0f),
-            Err(e) => eprintln!("{:?}", e),
+            Err(e) => {
+                eprintln!("{}, {:?}", line, &e);
+                return Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", e)));
+            }
         }
     }
     Ok(())
